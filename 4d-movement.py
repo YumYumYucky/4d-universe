2ce16824aef7c226260dc11b4229fc44a12ec3a8
YumYumYucky <96030192+YumYumYucky@users.noreply.github.com>
2022-02-02 21:48:47 -0500

    changed to 4d-movement.py and README.md

--- 4d-movement.py
+++ 4d-movement.py
@@ -13,13 +13,13 @@ from OpenGL.GLU import *
 
 
 
-global x_p, y_p, z_p, w_p, theta, l, x, y, z, w, direction
+global x_p, y_p, z_p, w_p, theta, l, x, y, z, w, direction, r, theta, psi, omega
 
 x_p=[1,1,1,1,-1,-1,-1,-1,1,1,1,1,-1,-1,-1,-1,0]
 y_p=[-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,0]
 z_p=[-1,-1,1,1,-1,-1,1,1,-1,-1,1,1,-1,-1,1,1,0]
 w_p=[1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,2]
-direction=[1,1,1,1]
+direction=[0,0,0,1]
 x=0
 y=0
 z=0
@@ -192,85 +192,89 @@ class move:
     print(f'{direction}')
 
 
+  def c_p(d):
+   global r, theta, psi, omega
+
+   r=math.sqrt(d[0]**2+d[1]**2+d[2]**2+d[3]**2)
+   theta=math.acos(d[3]/r)
+   psi=math.acos(d[2]/math.sqrt(d[0]**2+d[1]**2+d[2]**2))
+   omega=math.atan(d[0]/d[1])
+
+   print(f'magnatude= {r}')
+   print(f'theta= {theta}')
+   print(f'psi= {psi}')
+   print(f'omega= {omega}')
+
+
 
   def press():
    global x, y, z, w
- 
+   rotate='rotate'
+   counter_rotate='counter_rotate'
 
-   #just have one function that takes the input of the two axis of rotation and then plugs those into the function and have the same axis but fliped for counter rotate
   
    key=pygame.key.get_pressed()
    if key[pygame.K_j]:
     plane=[0,1]
-    rotate_first_plane(x_p,y_p,plane)
-    rotate_second_plane(x_p,y_p,plane)
+    rotate_first_plane(x_p,y_p,plane,rotate)
 
 
    if key[pygame.K_l]:
     plane=[0,1]
-    rotate_first_plane(y_p,x_p,plane)
-    rotate_second_plane(y_p,x_p,plane)
+    rotate_first_plane(y_p,x_p,plane,counter_rotate)
 
 
    if key[pygame.K_i]:
     plane=[0,2]
-    rotate_first_plane(x_p,z_p,plane)
-    rotate_second_plane(x_p,z_p,plane)
+    rotate_first_plane(x_p,z_p,plane,rotate)
 
 
    if key[pygame.K_k]:
     plane=[0,2]
-    rotate_first_plane(z_p,x_p,plane)
-    rotate_second_plane(z_p,x_p,plane)
+    rotate_first_plane(z_p,x_p,plane,counter_rotate)
 
 
 
    if key[pygame.K_t]:
     plane=[0,3]
-    rotate_first_plane(x_p,w_p,plane)
-    rotate_second_plane(x_p,w_p,plane)
+    rotate_first_plane(x_p,w_p,plane,rotate)
 
 
    if key[pygame.K_u]:
     plane=[0,3]
-    rotate_first_plane(w_p,x_p,plane)
-    rotate_second_plane(w_p,x_p,plane)
+    rotate_first_plane(w_p,x_p,plane,counter_rotate)
 
 
 
    if key[pygame.K_y]:
     plane=[1,2]
-    rotate_first_plane(y_p,z_p,plane)
-    rotate_second_plane(y_p,z_p,plane)
+    rotate_first_plane(y_p,z_p,plane,rotate)
 
 
    if key[pygame.K_h]:
     plane=[1,2]
-    rotate_first_plane(z_p,y_p,plane)
-    rotate_second_plane(z_p,y_p,plane)
+    rotate_first_plane(z_p,y_p,plane,counter_rotate)
 
 
 
    if key[pygame.K_f]:
     plane=[1,3]
-    rotate_first_plane(y_p,w_p,plane)
-    rotate_second_plane(y_p,w_p,plane)
+    rotate_first_plane(y_p,w_p,plane,rotate)
+
 
    if key[pygame.K_v]:
     plane=[1,3]
-    rotate_first_plane(w_p,y_p,plane)
-    rotate_second_plane(w_p,y_p,plane)
+    rotate_first_plane(w_p,y_p,plane,counter_rotate)
 
 
    if key[pygame.K_c]:
     plane=[2,3]
-    rotate_first_plane(z_p,w_p,plane)
-    rotate_second_plane(z_p,w_p,plane)
+    rotate_first_plane(z_p,w_p,plane,rotate)
+)
 
    if key[pygame.K_b]:
     plane=[2,3]
-    rotate_first_plane(w_p,z_p,plane)
-    rotate_second_plane(w_p,z_p,plane)
+    rotate_first_plane(w_p,z_p,plane,counter_rotate)
 
 
 
@@ -306,104 +310,136 @@ class move:
 
 
 #rotates any plane based on the input
-def rotate_first_plane(first_axis,second_axis,plane):
- global x_p, y_p, z_p, w_p
+def rotate_first_plane(first_axis,second_axis,plane, rotation):
+ global x_p, y_p, z_p, w_p, direction
 
 
  location=[]
+ s_location=[]
 
  #point0
  f0=(first_axis[0]*math.cos(theta))-(second_axis[0]*math.sin(theta))
+ s0=(first_axis[0]*math.sin(theta))+(second_axis[0]*math.cos(theta))
 
+ s_location.append(s0)
  location.append(f0)
 
  #point1
  f1=(first_axis[1]*math.cos(theta))-(second_axis[1]*math.sin(theta))
+ s1=(first_axis[1]*math.sin(theta))+(second_axis[1]*math.cos(theta))
 
+ s_location.append(s1)
  location.append(f1)
 
  #point2
  f2=(first_axis[2]*math.cos(theta))-(second_axis[2]*math.sin(theta))
+ s2=(first_axis[2]*math.sin(theta))+(second_axis[2]*math.cos(theta))
 
+ s_location.append(s2)
  location.append(f2)
 
  #point3
  f3=(first_axis[3]*math.cos(theta))-(second_axis[3]*math.sin(theta))
+ s3=(first_axis[3]*math.sin(theta))+(second_axis[3]*math.cos(theta))
 
+ s_location.append(s3)
  location.append(f3)
 
  #point4
  f4=(first_axis[4]*math.cos(theta))-(second_axis[4]*math.sin(theta))
+ s4=(first_axis[4]*math.sin(theta))+(second_axis[4]*math.cos(theta))
 
+ s_location.append(s4)
  location.append(f4)
 
  #point5
  f5=(first_axis[5]*math.cos(theta))-(second_axis[5]*math.sin(theta))
+ s5=(first_axis[5]*math.sin(theta))+(second_axis[5]*math.cos(theta))
 
+ s_location.append(s5)
  location.append(f5)
 
  #point6
  f6=(first_axis[6]*math.cos(theta))-(second_axis[6]*math.sin(theta))
+ s6=(first_axis[6]*math.sin(theta))+(second_axis[6]*math.cos(theta))
 
+ s_location.append(s6)
  location.append(f6)
 
  #point7
  f7=(first_axis[7]*math.cos(theta))-(second_axis[7]*math.sin(theta))
+ s7=(first_axis[7]*math.sin(theta))+(second_axis[7]*math.cos(theta))
 
+ s_location.append(s7)
  location.append(f7)
 
  #point8
  f8=(first_axis[8]*math.cos(theta))-(second_axis[8]*math.sin(theta))
+ s8=(first_axis[8]*math.sin(theta))+(second_axis[8]*math.cos(theta))
 
+ s_location.append(s8)
  location.append(f8)
 
  #point9
  f9=(first_axis[9]*math.cos(theta))-(second_axis[9]*math.sin(theta))
+ s9=(first_axis[9]*math.sin(theta))+(second_axis[9]*math.cos(theta))
 
+ s_location.append(s9)
  location.append(f9)
 
  #point10
  f10=(first_axis[10]*math.cos(theta))-(second_axis[10]*math.sin(theta))
+ s10=(first_axis[10]*math.sin(theta))+(second_axis[10]*math.cos(theta))
 
+ s_location.append(s10)
  location.append(f10)
 
  #point11
  f11=(first_axis[11]*math.cos(theta))-(second_axis[11]*math.sin(theta))
+ s11=(first_axis[11]*math.sin(theta))+(second_axis[11]*math.cos(theta))
 
+ s_location.append(s11)
  location.append(f11)
 
  #point12
  f12=(first_axis[12]*math.cos(theta))-(second_axis[12]*math.sin(theta))
+ s12=(first_axis[12]*math.sin(theta))+(second_axis[12]*math.cos(theta))
 
+ s_location.append(s12)
  location.append(f12)
 
  #point13
  f13=(first_axis[13]*math.cos(theta))-(second_axis[13]*math.sin(theta))
+ s13=(first_axis[13]*math.sin(theta))+(second_axis[13]*math.cos(theta))
 
+ s_location.append(s13)
  location.append(f13)
 
  #point14
  f14=(first_axis[14]*math.cos(theta))-(second_axis[14]*math.sin(theta))
+ s14=(first_axis[14]*math.sin(theta))+(second_axis[14]*math.cos(theta))
 
+ s_location.append(s14)
  location.append(f14)
 
  #point15
  f15=(first_axis[15]*math.cos(theta))-(second_axis[15]*math.sin(theta))
+ s15=(first_axis[15]*math.sin(theta))+(second_axis[15]*math.cos(theta))
 
+ s_location.append(s15)
  location.append(f15)
 
  #point16
  f16=(first_axis[16]*math.cos(theta))-(second_axis[16]*math.sin(theta))
+ s16=(first_axis[16]*math.sin(theta))+(second_axis[16]*math.cos(theta))
 
+ s_location.append(s16)
  location.append(f16)
 
 
 
  #up dating points and computing direction
 
- #print(f'plane[0]={plane[0]} and plane[1]={plane[1]}')
- direction[plane[0]]=(direction[plane[0]]*math.cos(theta))-(direction[plane[1]]*math.sin(theta))
- direction[plane[1]]=(direction[plane[0]]*math.sin(theta))+(direction[plane[1]]*math.cos(theta))
 
  if first_axis == x_p:
   x_p=location
@@ -418,125 +454,33 @@ def rotate_first_plane(first_axis,second_axis,plane):
   w_p=location
 
 
-# if second_axis == x_p:
-#  x_p=location
-
-# if second_axis == y_p:
-#  y_p=location
-
-# if second_axis == z_p:
-#  z_p=location
-
-# if second_axis == w_p:
-#  w_p=location
-
-
-
-
-
-
-def rotate_second_plane(first_axis,second_axis,plane):
- global x_p, y_p, z_p, w_p
-
-
- location=[]
-
- #point0
- s0=(first_axis[0]*math.sin(theta))+(second_axis[0]*math.cos(theta))
-
- location.append(s0)
-
- #point1
- s1=(first_axis[1]*math.sin(theta))+(second_axis[1]*math.cos(theta))
-
- location.append(s1)
-
- #point2
- s2=(first_axis[2]*math.sin(theta))+(second_axis[2]*math.cos(theta))
-
- location.append(s2)
-
- #point3
- s3=(first_axis[3]*math.sin(theta))+(second_axis[3]*math.cos(theta))
-
- location.append(s3)
-
- #point4
- s4=(first_axis[4]*math.sin(theta))+(second_axis[4]*math.cos(theta))
-
- location.append(s4)
-
- #point5
- s5=(first_axis[5]*math.sin(theta))+(second_axis[5]*math.cos(theta))
-
- location.append(s5)
-
- #point6
- s6=(first_axis[6]*math.sin(theta))+(second_axis[6]*math.cos(theta))
-
- location.append(s6)
-
- #point7
- s7=(first_axis[7]*math.sin(theta))+(second_axis[7]*math.cos(theta))
-
- location.append(s7)
-
- #point8
- s8=(first_axis[8]*math.sin(theta))+(second_axis[8]*math.cos(theta))
-
- location.append(s8)
-
- #point9
- s9=(first_axis[9]*math.sin(theta))+(second_axis[9]*math.cos(theta))
-
- location.append(s9)
-
- #point10
- s10=(first_axis[10]*math.sin(theta))+(second_axis[10]*math.cos(theta))
-
- location.append(s10)
-
- #point11
- s11=(first_axis[11]*math.sin(theta))+(second_axis[11]*math.cos(theta))
-
- location.append(s11)
-
- #point12
- s12=(first_axis[12]*math.sin(theta))+(second_axis[12]*math.cos(theta))
-
- location.append(s12)
-
- #point13
- s13=(first_axis[13]*math.sin(theta))+(second_axis[13]*math.cos(theta))
-
- location.append(s13)
-
- #point14
- s14=(first_axis[14]*math.sin(theta))+(second_axis[14]*math.cos(theta))
-
- location.append(s14)
-
- #point15
- s15=(first_axis[15]*math.sin(theta))+(second_axis[15]*math.cos(theta))
-
- location.append(s15)
-
- #point16
- s16=(first_axis[16]*math.sin(theta))+(second_axis[16]*math.cos(theta))
-
- location.append(s16)
-
  if second_axis == x_p:
-  x_p=location
+  x_p=s_location
 
  if second_axis == y_p:
-  y_p=location
+  y_p=s_location
 
  if second_axis == z_p:
-  z_p=location
+  z_p=s_location
 
  if second_axis == w_p:
-  w_p=location
+  w_p=s_location
+
+
+
+ if rotation=='rotate':
+  #print(f'plane[0]={plane[0]} and plane[1]={plane[1]}')
+  direction[plane[0]]=(direction[plane[0]]*math.cos(theta))-(direction[plane[1]]*math.sin(theta))
+  direction[plane[1]]=(direction[plane[0]]*math.sin(theta))+(direction[plane[1]]*math.cos(theta))
+
+ if rotation=='counter_rotate':
+  direction[plane[0]]=(direction[plane[0]]*math.cos(theta))+(direction[plane[1]]*math.sin(theta))
+  direction[plane[1]]=-(direction[plane[0]]*math.sin(theta))+(direction[plane[1]]*math.cos(theta))
+
+
+
+
+
 
 
 
@@ -556,8 +500,6 @@ def positions1():
 
 
 
-
-
 def Cube():
     glBegin(GL_LINES)
     for edge in edges1:
@@ -570,10 +512,15 @@ def Cube():
 
 def main():
     pygame.init()
+
+    glClearColor(0.2, 0.3, 0.3, 1.0);
+  
     display = (800,600)
+
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    gluPerspective(40, (display[0]/display[1]), 0.1, 50.0)
+
+    gluPerspective(40, (display[0]/display[1]), 0.1, 25.0)
 
     glTranslatef(0.0,0.0, -5)
 
@@ -587,7 +534,7 @@ def main():
         t1=threading.Thread(target=move.press())
         t2=threading.Thread(target=move.steriographic())
         t4=threading.Thread(target=move.positions())
-
+        t5=threading.Thread(target=move.c_p(direction))
 
 
         t1.start()
@@ -595,9 +542,10 @@ def main():
         #steriograhic projection
         t2.start()
 
-
         #orthographic projection
         #t3.start()
+
+        t5.start()
 
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # 4d-universe
+
+
+
+creates a hypercube with a hyper pyramid on one side of it.
+
+rotates the hyper cube along two axis
+
+j- rotates xy plane
+l- counter-rotates xy plane
+
+i- rotates xz plane
+k- counter-rotates xz plane
+
+t- rotates xw plane
+u- counter-rotates xw plane
+
+y- rotates yz plane
+h- counter-rotates yz plane
+
+f- rotates yw plane
+v- counter-rotates yw plane
+
+c- rotates zw plane
+b- counter-rotates zw plane
+
+
+The hyper cube can also be moved
+
+d- x higher
+a- x lower
+
+w- y higher
+s- y lower
+
+UP- z higher
+DOWN- z lower
+
+RIGHT- w higher
+LEFT- w lower
+
+
+Uses a steriographic projection to display the hyper cube
+
+Uses OpenGl to display the hyper cube
+
+
+Trying to add that the movement goes in the direction that the hypercube is facing the hyper pyramid being the direction that it is facing.
+Try to give the cube a velocity that can be updated every unit of time.
+Try to impliment gravity having the source be at 0,0,0,0 can have a velocity vetor that faces twords the orgin and magnatude of 1/r^3 so it is like 4D gravity
+Try to Make a hyper sphere around the orgin and have collision when the hyper cube goes into it. 
+
+
+
+
+new.py              moves a point around 4d space
+polar.py            convers a polar cordinate system into a cartesian cordinate system of the other way around
+opengltest.py       was the first try to impliment rotataion of a hyper cube in 4d
+OpenGlrotation.py   was the second time implimenting rotataion of a hyper cube but more efficient
+gametime.py         Attempt to make a game timer
+
